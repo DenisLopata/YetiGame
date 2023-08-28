462a50ac22cfbbe939945a4610d45f2deb0a8108
Mladen Perišić <mladen.perisic@comminus.hr>
2023-08-28 20:44:06 +0200

    first commit

---
+++ Scenes/Entities/PlayerBody.gd
@@ -0,0 +1,127 @@
+extends CharacterBody2D
+
+@export var speed_max := 675.0
+@export var acceleration := 1500.0
+@export var angular_speed_max := deg_to_rad(150)
+@export var angular_acceleration := deg_to_rad(1200)
+@export var drag_factor := 0.05
+@export var angular_drag_factor := 0.1
+
+@onready var angular_velocity : float = 0.0
+@onready var trail_full := preload("res://Assets/Trails/trail_full.png")
+@onready var trail_end := preload("res://Assets/Trails/trail_end.png")
+
+@export var acceleration_simple := 0.5
+const SPEED: float = 300.0
+const JUMP_VELOCITY: float = -400.0
+var count: int = 0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+# A factor that controls the character's inertia.
+@export var friction: float = 0.18
+@onready var trail_node := $Trail as Marker2D
+@onready var trail_sprite := $Trail/TrailSprite as Sprite2D
+
+#region movement
+func _physics_process(delta: float) -> void:
+	move(delta)
+
+func move(delta: float) -> void:
+#	move_spaceship(delta)
+	var speed = move_simple(delta)
+	add_trail(speed)
+
+func add_trail(speed: int) -> void:
+	
+	print(speed)
+	
+	#remove old children
+	var trail_children = trail_node.get_child_count()
+	if(trail_children > 0):
+		for child in trail_node.get_children():
+			child.queue_free()
+	trail_sprite = Sprite2D.new()
+	trail_node.add_child(trail_sprite)
+		
+	#add new trail
+	if speed == 0:
+		trail_sprite.visible = false
+	elif speed <= 100:
+		trail_sprite.visible = true
+		trail_sprite.position = Vector2(0, - 16)
+		trail_sprite.texture = trail_end
+	elif speed > 100 && speed <= 200:
+		trail_sprite.visible = true
+		trail_sprite.position = Vector2(0, - 16)
+		trail_sprite.texture = trail_full
+		var trail_sprite2 = trail_sprite.duplicate()
+		trail_sprite2.position = Vector2(0, - 32)
+		trail_sprite2.texture = trail_end
+		$Trail.add_child(trail_sprite2)
+	elif speed > 200:
+		trail_sprite.visible = true
+		trail_sprite.position = Vector2(0, - 16)
+		trail_sprite.texture = trail_full
+		var trail_sprite2 = trail_sprite.duplicate()
+		trail_sprite2.position = Vector2(0, - 32)
+		trail_sprite2.texture = trail_full
+		$Trail.add_child(trail_sprite2)
+		var trail_sprite3 = trail_sprite.duplicate()
+		trail_sprite3.position = Vector2(0, - 48)
+		trail_sprite3.texture = trail_end
+		$Trail.add_child(trail_sprite3)
+			
+		
+		
+#	trail_sprite.position = Vector2(0, - 16)
+#	trail_sprite.texture = trail_full
+#	var trail_sprite2 = trail_sprite.duplicate()
+#	trail_sprite2.position = Vector2(0, - 32)
+#	trail_sprite2.texture = trail_end
+#	add_child(trail_sprite2)
+
+func move_simple(delta: float) -> int:
+#
+	var direction := Vector2(
+		Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"),
+		Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	)
+	if direction.length() > 1.0:
+		direction = direction.normalized()
+#
+	# Using the follow steering behavior.
+	var target_velocity = direction * SPEED
+	var old_velocity = (target_velocity - velocity) * friction
+	velocity += (target_velocity - velocity) * friction
+	var speed := int(velocity.distance_to(old_velocity))
+	
+	move_and_slide()
+	return speed
+	
+func move_spaceship(delta: float) -> void:
+	velocity = velocity.limit_length(speed_max)
+
+	angular_velocity = clamp(angular_velocity, -angular_speed_max, angular_speed_max)
+	angular_velocity = lerp(angular_velocity, 0.0, angular_drag_factor)
+
+	move_and_slide()
+	rotation += angular_velocity * delta
+
+	var movement := _get_movement()
+
+	if is_equal_approx(movement.y, 0):
+		velocity = (velocity.lerp(Vector2.ZERO, drag_factor))
+
+	var direction := Vector2.UP.rotated(rotation)
+
+	velocity += movement.y * direction * acceleration * delta
+	angular_velocity += movement.x * angular_acceleration * delta
+	
+func _get_movement() -> Vector2:
+	return Vector2(
+		Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"),
+		Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	)
+#endregion
+
---
+++ Scenes/MainScenes/Map1.gd
@@ -0,0 +1,125 @@
+class_name Map1
+extends WorldGenerator
+
+var background_tiles: Array[Vector2i] = [Vector2i(0,0)
+, Vector2i(1,1)
+, Vector2i(5,0)
+, Vector2i(4,1)
+, Vector2i(1,4)
+, Vector2i(4,4)]
+
+## The Flags
+@export var Flag: PackedScene
+
+@onready var _screen_size = get_viewport_rect().size
+## The number of asteroids to place in each sector.
+@export var asteroid_density := 3
+@onready var player = $Player/PlayerBody
+@onready var ground = $Ground as TileMap
+@onready var path = $Path as TileMap
+
+func _ready() -> void:
+	Flag = load("res://Scenes/Entities/Flag.tscn")
+	generate()
+	generate_tiles_around_player()
+
+
+func _process(delta):
+	pass
+
+func _physics_process(_delta: float) -> void:
+	# Every frame, we compare the player's position to the current sector. If
+	# they move far enough from it, we need to update the world.
+	var sector_location := _current_sector * sector_size
+	var distance = player.global_position.distance_squared_to(sector_location)
+	
+	if player.global_position.distance_squared_to(sector_location) > _sector_size_squared:
+		# Our function to update the sectors takes a vector to offset. As the
+		# player can be moving left, right, up, or down, we store that
+		# information in our sector_offset variable.
+		var sector_offset := Vector2.ZERO
+		sector_offset = (player.global_position - sector_location) / sector_size
+		sector_offset.x = int(sector_offset.x)
+		sector_offset.y = int(sector_offset.y)
+		_update_sectors(sector_offset)
+		generate_tiles_around_player()
+		print("--------DONE GENERATE------------")
+	
+func generate_tiles_around_player() -> void:
+	
+		var glbl = player.global_position
+		var tile_pos = ground.local_to_map(glbl)
+		
+		var path_coords := []
+		var path_width := 5
+		var cnt = 0
+		for x in range(- 100, 100):
+			for y in range( - 100, 100):
+				var cell_rect := Vector2(
+						tile_pos.x + x,
+						tile_pos.y + y
+					)
+#				ground.set_cell(0, cell_rect, 1, Vector2(1,1))
+				var rng_bckg_tile = background_tiles[randi()%background_tiles.size()]
+				ground.set_cell(0, cell_rect, 1, rng_bckg_tile)
+				if x >= -path_width and x <= path_width:
+					path_coords.append(Vector2(x, cell_rect.y))
+					if cnt > 100 and cnt < 150:
+						path_coords.append(Vector2(path_width + 1, cell_rect.y))
+					if cnt > 103 and cnt < 148:
+						path_coords.append(Vector2(path_width + 2, cell_rect.y))
+					
+					cnt = cnt + 1
+		path.set_cells_terrain_connect(0, path_coords, 0, 0)
+				
+
+	
+func generate_collision_path_around_player(x: int, cell_rect: Vector2) -> void:
+		var path_coords := []
+		var path_width := 5
+		if x >= -path_width and x <= path_width:
+			if x == -path_width:
+					path.set_cell(0, Vector2(x, cell_rect.y), 0, Vector2i(1,0))
+			elif x == path_width:
+					path.set_cell(0, Vector2(x, cell_rect.y), 0, Vector2i(4,0))
+			else:
+					path.set_cell(0, Vector2(x, cell_rect.y), 0, Vector2i(2,0))
+	
+# Generates items and places them inside
+# of the sector's bounds with a random position, rotation, and scale.
+func _generate_sector(x_id: int, y_id: int) -> void:
+	pass
+#	_rng.seed = make_seed_for(x_id, y_id)
+	
+	# List of entities generated in this sector.
+	var sector_data := []
+#	var position = _generate_random_position(x_id, y_id)
+	for _i in range(asteroid_density):
+		var flag := Flag.instantiate()
+		add_child(flag)
+	
+		# We generate a random position for each asteroid within the rectangle's bounds.
+		flag.position = _generate_random_position(x_id, y_id)
+#		flag.rotation = _rng.randf_range(-PI, PI)
+#		flag.scale *= _rng.randf_range(0.2, 1.0)
+		sector_data.append(flag)
+
+	# We store references to all asteroids to free them later.
+	_sectors[Vector2(x_id, y_id)] = sector_data
+
+# Returns a random position within the sector's bounds, given the sector's coordinates.
+func _generate_random_position(x_id: int, y_id: int) -> Vector2:
+	# Calculate the sector boundaries based checked the current x and y sector
+	# coordinates.
+	var sector_position = Vector2(x_id * sector_size, y_id * sector_size)
+	var sector_top_left = Vector2(
+		sector_position.x - _half_sector_size, sector_position.y - _half_sector_size
+	)
+	var sector_bottom_right = Vector2(
+		sector_position.x + _half_sector_size, sector_position.y + _half_sector_size
+	)
+	return Vector2(
+		_rng.randf_range(sector_top_left.x, sector_bottom_right.x),
+		_rng.randf_range(sector_top_left.y, sector_bottom_right.y)
+	)
+	
